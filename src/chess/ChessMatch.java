766e31c333d96dad413c81747c1f06f11304b921
Ana Eliza <anaelizaperobelli@gmail.com>
2023-06-10 20:21:44 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,6 +25,10 @@ public class ChessMatch {
 		 return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
 		board.placePiece(new Rook(board, Color.WHITE), new Position(9, 1));
 		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
